538bc296cb621f15d2ecb415eac5b94030b01514
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2023-10-29 18:16:13 +0800

    Add `Orbit` class

---
+++ ZeroV.Game/Elements/Orbit.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+
+using osuTK;
+
+namespace ZeroV.Game.Elements;
+
+internal partial class Orbit : CompositeDrawable {
+    private Container? container;
+    private Box? innerBox;
+    private Box? innerLine;
+
+    // TODO:
+    //public override Single Height { get;set;
+    //}
+
+    public Orbit() {
+        this.AutoSizeAxes = Axes.Both;
+        this.Origin = Anchor.Centre;
+    }
+
+    [BackgroundDependencyLoader]
+    private void load() {
+        this.innerBox = new Box {
+            Origin = Anchor.BottomCentre,
+            Anchor = Anchor.BottomCentre,
+            Colour = Colour4.Black,
+            Size = new Vector2(100, 768),
+        };
+        this.innerLine = new Box {
+            Origin = Anchor.BottomCentre,
+            Anchor = Anchor.BottomCentre,
+            Colour = Colour4.White,
+            EdgeSmoothness = new Vector2(3, 0),
+            Size = new Vector2(4, 768),
+        };
+
+        this.container = new Container() {
+            AutoSizeAxes = Axes.Both,
+            Origin = Anchor.BottomCentre,
+            Anchor = Anchor.BottomCentre,
+            Children = new Drawable[] {
+                this.innerBox,
+                this.innerLine,
+            }
+        };
+        this.InternalChild = this.container;
+    }
+}
--- ZeroV.Game/MainScreen.cs
+++ ZeroV.Game/MainScreen.cs
@@ -1,9 +1,9 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Screens;
 
+using osuTK;
 using osuTK.Graphics;
 
 using ZeroV.Game.Elements;
@@ -16,21 +16,32 @@ public partial class MainScreen : Screen {
         this.InternalChildren = new Drawable[] {
             new Box {
                 Colour = Color4.Violet,
+                // 1366 * 768
                 RelativeSizeAxes = Axes.Both,
             },
-            new SpriteText {
-                Y = 20,
-                Text = "Main Screen",
-                Anchor = Anchor.TopCentre,
-                Origin = Anchor.TopCentre,
-                Font = FontUsage.Default.With(size: 40)
+            new Box {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Size = new Vector2(1366, 768)
             },
+            //new SpriteText {
+            //    Y = 20,
+            //    Text = "Main Screen",
+            //    Anchor = Anchor.TopCentre,
+            //    Origin = Anchor.TopCentre,
+            //    Font = FontUsage.Default.With(size: 40)
+            //},
             //new SpinningBox {
             //    Anchor = Anchor.Centre,
             //}
-            new BlinkParticles {
-                Anchor = Anchor.Centre
-            }
+            new Orbit {
+                Anchor = Anchor.Centre,
+                //Height = 1024
+            },
+            //new BlinkParticles
+            //{
+            //    Anchor = Anchor.Centre
+            //}
         };
     }
 }
