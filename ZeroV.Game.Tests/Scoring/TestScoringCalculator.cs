14a2c0f760a457d57de003f802543bd1a18a5aa6
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2023-11-23 01:02:48 +0800

    Create TestScoringCalculator.cs

---
+++ ZeroV.Game.Tests/Scoring/TestScoringCalculator.cs
@@ -0,0 +1,37 @@
+using System;
+
+using NUnit.Framework;
+
+using ZeroV.Game.Scoring;
+
+namespace ZeroV.Game.Tests.Scoring;
+
+[TestFixture]
+internal class TestScoringCalculator {
+    private ScoringCalculator calculator1024;
+    private ScoringCalculator calculator4;
+
+    [SetUp]
+    public void SetUp() {
+        this.calculator1024 = new ScoringCalculator(1024);
+        this.calculator4 = new ScoringCalculator(4);
+    }
+
+    [Test]
+    public void Test1024() {
+        for (var i = 0; i < 1024; i++) {
+            this.calculator1024.AddTarget(TargetResult.Perfect);
+            Console.WriteLine(this.calculator1024.DisplayScoring);
+        }
+        Assert.IsTrue(this.calculator1024.DisplayScoring is 1_000_000);
+    }
+
+    [Test]
+    public void Test4() {
+        for (var i = 0; i < 4; i++) {
+            this.calculator4.AddTarget(TargetResult.Perfect);
+            Console.WriteLine(this.calculator4.DisplayScoring);
+        }
+        Assert.IsTrue(this.calculator4.DisplayScoring is 1_000_000);
+    }
+}
