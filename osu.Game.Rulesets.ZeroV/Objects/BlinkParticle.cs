34bbe7bee7be6e7f301e545471969f8523f17373
jing8956 <jing8956@outlook.com>
2023-12-19 18:20:45 +0800

    Add BlinkParticle

---
+++ osu.Game.Rulesets.ZeroV/Objects/BlinkParticle.cs
@@ -0,0 +1,8 @@
+﻿using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.ZeroV.Judgements;
+
+namespace osu.Game.Rulesets.ZeroV.Objects;
+
+public class BlinkParticle : ZeroVHitObject {
+    public override Judgement CreateJudgement() => new ZeroVJudgement();
+}
