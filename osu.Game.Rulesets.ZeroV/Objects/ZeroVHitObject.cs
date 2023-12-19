0c34cfe4e8508b0d66c7e1d1d6df105379dfed5a
jing8956 <jing8956@outlook.com>
2023-12-19 18:20:31 +0800

    Update ZeroVHitObject

--- osu.Game.Rulesets.ZeroV/Objects/ZeroVHitObject.cs
+++ osu.Game.Rulesets.ZeroV/Objects/ZeroVHitObject.cs
@@ -1,16 +1,26 @@
 using System;
 
-using osu.Game.Rulesets.Judgements;
+using osu.Framework.Bindables;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets.ZeroV.Scoring;
 
 namespace osu.Game.Rulesets.ZeroV.Objects;
 
-public class ZeroVHitObject : HitObject {
+public abstract class ZeroVHitObject : HitObject, IHasColumn {
 
     /// <summary>
     /// Range = [-1,1]
     /// </summary>
     public Int32 Lane;
 
-    public override Judgement CreateJudgement() => new();
+    private HitObjectProperty<Int32> column;
+    public Bindable<Int32> ColumnBindable => this.column.Bindable;
+    public virtual Int32 Column {
+        get => this.column.Value;
+        set => this.column.Value = value;
+    }
+
+    protected override HitWindows CreateHitWindows() => new ZeroVHitWindows();
 }
