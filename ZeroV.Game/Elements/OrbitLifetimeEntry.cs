5bf668951ee35ccfa91baa709b56376615e27766
jing8956 <jing8956@outlook.com>
2024-02-08 00:14:31 +0800

    Update OrbitLifetimeEntry rename constructor parameter

--- ZeroV.Game/Elements/OrbitLifetimeEntry.cs
+++ ZeroV.Game/Elements/OrbitLifetimeEntry.cs
@@ -4,9 +4,9 @@ namespace ZeroV.Game.Elements;
 
 public class OrbitLifetimeEntry : ZeroVObjectLifetimeEntry<Orbit, OrbitDrawable>
 {
-    public OrbitLifetimeEntry(Orbit @object) : base(@object)
+    public OrbitLifetimeEntry(Orbit orbit) : base(orbit)
     {
-        this.LifetimeStart = @object.StartTime;
-        this.LifetimeEnd = @object.EndTime;
+        this.LifetimeStart = orbit.StartTime;
+        this.LifetimeEnd = orbit.EndTime;
     }
 }
