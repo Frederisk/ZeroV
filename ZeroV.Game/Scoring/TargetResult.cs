8d978e604f73a0cb470b86b815ee9da4b383b85a
jing8956 <jing8956@outlook.com>
2024-02-07 16:05:02 +0800

    Update TargetResult use flags

--- ZeroV.Game/Scoring/TargetResult.cs
+++ ZeroV.Game/Scoring/TargetResult.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace ZeroV.Game.Scoring;
 
+[Flags]
 public enum TargetResult {
-    Miss,
-    NormalEarly,
-    NormalLate,
-    PerfectEarly,
-    PerfectLate,
-    MaxPerfect,
+    Miss = 0b0000,
+    Normal = 0b0001,
+    Perfect = 0b0010,
+    MaxPerfect = 0b0100,
+
+    Early = 0b1000,
+
+    NormalEarly = Normal | Early,
+    NormalLate = Normal,
+    PerfectEarly = Perfect | Early,
+    PerfectLate = Perfect,
+
+    ResultMask = Normal | Perfect | MaxPerfect,
 }
