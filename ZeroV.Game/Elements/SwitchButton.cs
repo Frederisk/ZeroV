e0c18292e3f79b3c93f62da03019ee5e80cc54b1
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-02-23 17:25:31 +0800

    Update SwitchButton.cs

--- ZeroV.Game/Elements/SwitchButton.cs
+++ ZeroV.Game/Elements/SwitchButton.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
 
 using osuTK;
 
@@ -17,11 +11,11 @@ namespace ZeroV.Game.Elements;
 public partial class SwitchButton : Checkbox {
 
     public SwitchButton() {
-        this.Size = new Vector2(50, 25);
+        this.Size = new Vector2(150, 50);
     }
 
     [BackgroundDependencyLoader]
-    private void Load() {
+    private void load() {
         this.InternalChild = new Container {
             RelativeSizeAxes = Axes.Both,
             Children = [
@@ -30,18 +24,63 @@ public partial class SwitchButton : Checkbox {
                     Colour = Colour4.Yellow,
                 },
                 // Background
+                new BufferedContainer {
+                    RelativeSizeAxes = Axes.Both,
+                    Children = [
+                        new Box {
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = Colour4.Red,
+                        },
+                        new Container {
+                            Origin = Anchor.CentreLeft,
+                            Anchor = Anchor.CentreLeft,
+                            RelativeSizeAxes = Axes.Both,
+                            FillMode = FillMode.Fit,
+                            Child = new Box {
+                                Origin = Anchor.CentreLeft,
+                                Anchor = Anchor.CentreLeft,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.White,
+                                Size = new Vector2(0.5f, 1),
+                            },
+                            Blending = new BlendingParameters {
+                                SourceAlpha = BlendingType.Zero,
+                                DestinationAlpha = BlendingType.OneMinusSrcAlpha
+                            },
+                        },
+                        new Container {
+                            Origin = Anchor.CentreRight,
+                            Anchor = Anchor.CentreRight,
+                            RelativeSizeAxes = Axes.Both,
+                            FillMode = FillMode.Fit,
+                            Child = new Box {
+                                Origin = Anchor.CentreRight,
+                                Anchor = Anchor.CentreRight,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Transparent,
+                                Size = new Vector2(0.5f, 1),
+                            },
+                            Blending = new BlendingParameters {
+                                SourceAlpha = BlendingType.Zero,
+                                DestinationAlpha = BlendingType.OneMinusSrcAlpha
+                            },
+                        },
+                    ],
+                },
                 new Diamond {
                     Origin = Anchor.CentreLeft,
                     Anchor = Anchor.CentreLeft,
-                    Size = new Vector2(this.Size.Y),
+                    RelativeSizeAxes = Axes.Both,
+                    FillMode = FillMode.Fit,
                     Colour = Colour4.Black,
                 },
                 new Diamond {
                     Origin = Anchor.CentreRight,
                     Anchor = Anchor.CentreRight,
-                    Size = new Vector2(this.Size.Y),
+                    RelativeSizeAxes = Axes.Both,
+                    FillMode = FillMode.Fit,
                     Colour = Colour4.Black,
-                }
+                },
             ],
         };
     }
