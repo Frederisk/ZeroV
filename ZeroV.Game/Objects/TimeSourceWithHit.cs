c164ba7effce325eada567e141f909c702dd19b1
jing8956 <jing8956@outlook.com>
2024-02-27 15:46:01 +0800

    Remove HitWIndows

--- ZeroV.Game/Objects/TimeSourceWithHit.cs
+++ ZeroV.Game/Objects/TimeSourceWithHit.cs
@@ -1,7 +1,22 @@
+using System;
 using ZeroV.Game.Scoring;
 
 namespace ZeroV.Game.Objects;
 
 public abstract class TimeSourceWithHit : TimeSource {
-    public abstract HitWindows HitWindows { get; }
+
+    public TargetResult ResultFor(Double timeOffset) {
+        TargetResult isEarly = Double.IsNegative(timeOffset) ? TargetResult.Early : 0;
+        TargetResult result = this.ResultForInternal(timeOffset);
+
+        return result | isEarly;
+    }
+
+    protected virtual TargetResult ResultForInternal(Double timeOffset) =>
+        Double.Abs(timeOffset) switch {
+            <= 100.0 => TargetResult.MaxPerfect,
+            <= 500.0 => TargetResult.Perfect,
+            <= 1000.0 => TargetResult.Normal,
+            _ => TargetResult.Miss,
+        };
 }
