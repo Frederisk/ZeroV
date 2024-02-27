c1c8b153f69a51b34c3d262e9666cd04f23cf9d5
jing8956 <jing8956@outlook.com>
2024-02-27 17:15:37 +0800

    Add PressParticleSource, SlideParticleSource and StrokeParticleSource

---
+++ ZeroV.Game/Elements/Particles/PressParticleSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using ZeroV.Game.Objects;
+
+namespace ZeroV.Game.Elements.Particles;
+
+public class PressParticleSource(Double startTime, Double endTime) : TimeSourceWithHit {
+    public override Double StartTime => startTime;
+    public override Double EndTime => endTime;
+}
