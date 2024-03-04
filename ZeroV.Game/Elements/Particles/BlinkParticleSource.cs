30b602c52126915a7989b21b56120c62cfc51917
jing8956 <jing8956@outlook.com>
2024-03-04 15:16:44 +0800

    Update BlinkParticleSource

--- ZeroV.Game/Elements/Particles/BlinkParticleSource.cs
+++ ZeroV.Game/Elements/Particles/BlinkParticleSource.cs
@@ -5,6 +5,6 @@ using ZeroV.Game.Objects;
 namespace ZeroV.Game.Elements.Particles;
 
 public class BlinkParticleSource(Double startTime) : TimeSourceWithHit {
-    public override Double StartTime { get; } = startTime;
+    public override Double StartTime => startTime;
     public override Double EndTime => this.StartTime;
 }
