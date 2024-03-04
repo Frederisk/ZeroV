8def77ae816e54fcf63a9027b6c6e5bd9d1c5f77
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-03-04 14:48:09 +0800

    Add `ZeroVIcon`

---
+++ ZeroV.Game.Tests/Visual/Elements/TestSceneIcons.cs
@@ -0,0 +1,34 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+using osuTK;
+
+using ZeroV.Game.Graphics;
+
+namespace ZeroV.Game.Tests.Visual.Elements;
+
+public partial class TestSceneIcons : ZeroVTestScene {
+
+    [BackgroundDependencyLoader]
+    private void load() {
+        FillFlowContainer flower = new() {
+            Direction = FillDirection.Horizontal,
+        };
+        this.Child = flower;
+        //flower.Add(new SpriteIcon {
+        //    Size = new Vector2(20),
+        //    Icon = ZeroVIcon.RulesetOsu,
+        //});
+        flower.Add(
+            new ZeroVIcon {
+                IconType = ZeroIconType.Exit,
+                Size = new Vector2(100),
+            });
+        flower.Add(
+            new ZeroVIcon {
+                IconType = ZeroIconType.Exit,
+                Size = new Vector2(100),
+            });
+    }
+}
