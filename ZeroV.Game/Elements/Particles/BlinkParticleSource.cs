1de7d00d97c4d989baf279afa945b6577dad5db8
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-03-29 00:38:22 +0800

    Update BlinkParticleSource.cs

--- ZeroV.Game/Elements/Particles/BlinkParticleSource.cs
+++ ZeroV.Game/Elements/Particles/BlinkParticleSource.cs
@@ -8,15 +8,21 @@ public class BlinkParticleSource(Double startTime) : ParticleSource {
     public override Double StartTime => startTime;
     public override Double EndTime => this.StartTime;
 
-    //public override TargetResult Judge(in JudgeInput input) {
-    //    TargetResult result = Judgment.JudgeBlink(startTime, input.CurrentTime);
-    //    switch (input.IsTouchDown) {
-    //        case null or false when result != TargetResult.Miss:
-    //            result = TargetResult.None;
-    //            break;
-    //    }
+    public override TargetResult JudgeEnter(Double currentTime, Boolean isNewTouch) {
+        // base.JudgeEnter(currentTime, isNewTouch); // just return null
+        if (isNewTouch) {
+            TargetResult result = Judgment.JudgeBlink(this.StartTime, currentTime);
+            return result;
+        }
+        return TargetResult.None;
+    }
 
-    //    return result;
-    //}
-
+    public override TargetResult JudgeUpdate(Double currentTime) {
+        // base.JudgeUpdate(currentTime); // just return null
+        TargetResult result = Judgment.JudgeBlink(this.StartTime, currentTime);
+        if (result is TargetResult.Miss) {
+            return result;
+        }
+        return TargetResult.None;
+    }
 }
