0424c9ff9e1d5cda35fc85ef7008ca1b3e9acbd5
jing8956 <jing8956@outlook.com>
2024-07-14 20:49:47 +0800

    PlaySongSelect

--- ZeroV.Game.Tests/Visual/Screens/TestScenePlaySongSelectScreen.cs
+++ ZeroV.Game.Tests/Visual/Screens/TestScenePlaySongSelectScreen.cs
@@ -1,24 +1,90 @@
+using System;
+using System.Collections.Generic;
+
 using NUnit.Framework;
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Platform;
 using osu.Framework.Screens;
 
+using ZeroV.Game.Data;
+using ZeroV.Game.Objects;
 using ZeroV.Game.Screens;
 
 namespace ZeroV.Game.Tests.Visual.Screens;
 
 [TestFixture]
 public partial class TestScenePlaySongSelectScreen : ZeroVTestScene {
+    private ScreenStack screenStack = default!;
 
-    private readonly ScreenStack screenStack;
-
-    public TestScenePlaySongSelectScreen() {
-        this.Add(this.screenStack = new ScreenStack());
-        
+    [BackgroundDependencyLoader]
+    private void load(Storage storage) {
+        this.dependencies!.CacheAs<TrackInfoProvider>(new FakeTrackInfoProvider(storage));
+        this.Add(this.screenStack = new ScreenStack() { RelativeSizeAxes = Axes.Both });
     }
 
     [Test]
     public void CreateScreen() {
         this.screenStack.Push(new PlaySongSelectScreen() { RelativeSizeAxes = Axes.Both });
+    }
+
+    private DependencyContainer? dependencies;
+    protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
+        this.dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
+    private class FakeTrackInfoProvider(Storage storage) : TrackInfoProvider(storage) {
+        public override IReadOnlyList<TrackInfo> TrackInfoList => [
+            new() {
+                Title = "Test Title",
+                Album = "Test Album",
+                TrackOrder = 0,
+                Artists = "Test Artists",
+                FileOffset = TimeSpan.FromSeconds(1),
+                GameAuthor = "Test GameAuthor",
+                Description = "Test Description",
+                GameVersion = new Version(1,0,0),
+
+                Maps = [
+                    new MapInfo() {
+                        Difficulty = 1,
+                        MapOffset = TimeSpan.FromSeconds(2),
+                        BlinkCount = 1,
+                        PressCount = 1,
+                        SlideCount = 1,
+                        StrokeCount = 1
+                    },
+                    new MapInfo() {
+                        Difficulty = 2,
+                        MapOffset = TimeSpan.FromSeconds(2),
+                        BlinkCount = 1,
+                        PressCount = 1,
+                        SlideCount = 1,
+                        StrokeCount = 1
+                    }
+                ],
+                File = new System.IO.FileInfo("testFileName.xml")
+            },
+            new() {
+                Title = "Test Title2",
+                Album = "Test Album",
+                TrackOrder = 0,
+                Artists = "Test Artists",
+                FileOffset = TimeSpan.FromSeconds(1),
+                GameAuthor = "Test GameAuthor",
+                Description = "Test Description",
+                GameVersion = new Version(1,0,0),
+                Maps = [
+                    new MapInfo() {
+                        Difficulty = 1,
+                        MapOffset = TimeSpan.FromSeconds(2),
+                        BlinkCount = 1,
+                        PressCount = 1,
+                        SlideCount = 1,
+                        StrokeCount = 1
+                    }],
+                File = new System.IO.FileInfo("testFileName.xml")
+            }
+        ];
     }
 }
--- ZeroV.Game/Screens/PlaySongSelectScreen.cs
+++ ZeroV.Game/Screens/PlaySongSelectScreen.cs
@@ -4,10 +4,204 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
 using osu.Framework.Screens;
+
+using ZeroV.Game.Objects;
+using osuTK.Graphics;
+using osu.Framework.Graphics.Shapes;
+using ZeroV.Game.Data;
 
 namespace ZeroV.Game.Screens;
 
+[Cached]
 public partial class PlaySongSelectScreen : Screen {
+    private FillFlowContainer container = null!;
 
+    [Resolved]
+    private TrackInfoProvider beatmapWrapperProvider { get; set; } = null!;
+
+    [BackgroundDependencyLoader]
+    private void load() {
+        this.RelativeSizeAxes = Axes.Both;
+
+        this.container = new FillFlowContainer() {
+            RelativeSizeAxes = Axes.X,
+            AutoSizeAxes = Axes.Y,
+            Direction = FillDirection.Vertical,
+            Spacing = new osuTK.Vector2(0, 10),
+            Padding = new MarginPadding(10)
+        };
+
+        foreach (TrackInfo item in this.beatmapWrapperProvider.TrackInfoList) {
+            this.container.Add(new TrackInfoListItem(item));
+        }
+
+        var child = new BasicScrollContainer() {
+            RelativeSizeAxes = Axes.Both,
+            Child = this.container
+        };
+        this.AddInternal(child);
+    }
+
+    private MapInfoListItem? selectedItem;
+    public void OnSelect(MapInfoListItem item) {
+        this.selectedItem?.OnSelectCancel();
+        this.selectedItem = item;
+    }
+
+    private TrackInfoListItem? expandedItem;
+    public void OnExpanded(TrackInfoListItem item) {
+        if (this.expandedItem != null) {
+            this.expandedItem.IsExpanded = false;
+        }
+        this.expandedItem = item;
+    }
+
+    public void ConfirmSelect() {
+
+    }
+
+    [Cached]
+    public partial class TrackInfoListItem(TrackInfo info) : CompositeDrawable {
+        private Boolean isExpanded;
+        private FillFlowContainer container = null!;
+
+        public TrackInfo TrackInfo => info;
+
+        [BackgroundDependencyLoader]
+        private void load() {
+            this.Masking = true;
+            this.BorderColour = Color4.White;
+            this.RelativeSizeAxes = Axes.X;
+            this.AutoSizeAxes = Axes.Y;
+
+            this.container = new FillFlowContainer() {
+                RelativeSizeAxes = Axes.X,
+                Height = 0,
+                Direction = FillDirection.Vertical,
+                Spacing = new osuTK.Vector2(0, 0),
+                Margin = new MarginPadding(0) {
+                    Left = 100
+                }
+            };
+
+            this.AddInternal(new FillFlowContainer() {
+                RelativeSizeAxes = Axes.X,
+                AutoSizeAxes = Axes.Y,
+                Children = [
+                    new TrackInfoListItemHeader(),
+                    this.container
+                ]
+            });
+
+            foreach (MapInfo item in info.Maps) {
+                this.container.Add(new MapInfoListItem(item));
+            }
+        }
+
+        public Boolean IsExpanded {
+            get => this.isExpanded;
+            set {
+                if (value) {
+                    this.container.AutoSizeAxes = Axes.Y;
+                } else {
+                    this.container.AutoSizeAxes = Axes.None;
+                    this.container.Height = 0;
+                }
+                this.isExpanded = value;
+            }
+        }
+
+        public partial class TrackInfoListItemHeader : CompositeDrawable {
+
+            [Resolved]
+            private PlaySongSelectScreen songSelect { get; set; } = null!;
+
+            [Resolved]
+            private TrackInfoListItem listItem { get; set; } = null!;
+
+            [BackgroundDependencyLoader]
+            private void load() {
+                this.RelativeSizeAxes = Axes.X;
+                this.Height = 100;
+
+                this.AddInternal(new Box() {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.Blue
+                });
+                this.AddInternal(new SpriteText() {
+                    Origin = Anchor.TopCentre,
+                    Anchor = Anchor.TopCentre,
+                    Text = this.listItem.TrackInfo.Title,
+                    Colour = Color4.Black,
+                    Font = FontUsage.Default.With(size: 52)
+                });
+            }
+
+            protected override Boolean OnClick(ClickEvent e) {
+                this.listItem.IsExpanded = !this.listItem.IsExpanded;
+                if (this.listItem.IsExpanded) {
+                    this.songSelect.OnExpanded(this.listItem);
+                }
+                return base.OnClick(e);
+            }
+        }
+    }
+
+    public partial class MapInfoListItem(MapInfo mapInfo) : CompositeDrawable {
+        private Boolean selected;
+
+        [Resolved]
+        private PlaySongSelectScreen songSelect { get; set; } = null!;
+
+        [BackgroundDependencyLoader]
+        private void load() {
+            this.Masking = true;
+            this.BorderColour = Color4.White;
+            this.RelativeSizeAxes = Axes.X;
+            this.Height = 50;
+            this.Margin = new MarginPadding(0) {
+                Top = 5
+            };
+
+            this.AddInternal(new Box() {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Colour4.Green
+            });
+            this.AddInternal(new SpriteText() {
+                Origin = Anchor.TopCentre,
+                Anchor = Anchor.TopCentre,
+                Text = mapInfo.Difficulty.ToString(),
+                Colour = Color4.Black,
+                Font = FontUsage.Default.With(size: 52)
+            });
+        }
+
+        public void OnSelect() {
+            if (!this.selected) {
+                this.songSelect.OnSelect(this);
+
+                this.selected = true;
+                this.BorderThickness = 3;
+            } else {
+                //TODO: Confirm select
+                this.songSelect.ConfirmSelect();
+            }
+        }
+
+        public void OnSelectCancel() {
+            this.selected = false;
+            this.BorderThickness = 0;
+        }
+
+        protected override Boolean OnClick(ClickEvent e) {
+            this.OnSelect();
+            return base.OnClick(e);
+        }
+    }
 }
