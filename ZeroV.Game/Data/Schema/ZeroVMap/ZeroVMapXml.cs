f46aabbce6b30713bd0f3ead0c0ee0d18f72efa8
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-07-23 14:45:49 +0800

    Update ZeroVMapXml.cs

--- ZeroV.Game/Data/Schema/ZeroVMap/ZeroVMapXml.cs
+++ ZeroV.Game/Data/Schema/ZeroVMap/ZeroVMapXml.cs
@@ -75,7 +75,7 @@ public partial class TrackInfoXml {
     [AllowNull]
     [MaybeNull]
     [XmlElement("Artists")]
-    public Object? Artists { get; set; }
+    public String? Artists { get; set; }
 
     /// <summary>
     /// <para xml:lang="en">Pattern: -?([01]\d|2[0-3]):\d{2}:\d{2}(\.\d{1,7})?.</para>
