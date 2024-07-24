92289a99b1edd0c86cd49d540f40e5f0c6628182
jing8956 <jing8956@outlook.com>
2024-07-24 11:38:27 +0800

    Add PauseOverlay

---
+++ ZeroV.Game.Tests/Visual/Screens/TestScenePauseOverlay.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+
+using ZeroV.Game.Screens;
+
+namespace ZeroV.Game.Tests.Visual.Screens;
+
+[TestFixture]
+public partial class TestScenePauseOverlay : ZeroVTestScene {
+    private PauseOverlay overlay = default!;
+
+    [Test]
+    public void CreateOverlay() {
+        this.Add(new Box() { RelativeSizeAxes = Axes.Both, Colour = Colour4.White });
+
+#pragma warning disable IDE0028 // Use collection initializers or expressions
+        this.Add(this.overlay = new PauseOverlay());
+#pragma warning restore IDE0028 // Use collection initializers or expressions
+
+        this.overlay.Show();
+    }
+
+}
---
+++ ZeroV.Game/Screens/PauseOverlay.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input.Events;
+
+namespace ZeroV.Game.Screens;
+
+public partial class PauseOverlay : OverlayContainer {
+
+    protected const Int32 TRANSITION_DURATION = 200;
+    private const Single background_alpha = 0.75f;
+
+    [BackgroundDependencyLoader]
+    private void load() {
+        this.RelativeSizeAxes = Axes.Both;
+
+        this.Children = [
+            new Box() {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Colour4.Black,
+                Alpha = background_alpha,
+            },
+            new FillFlowContainer() {
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal,
+                Spacing = new osuTK.Vector2(10, 0),
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Children = [
+                    new BasicButton() {
+                        Height = 100,
+                        Width = 100,
+                        Text = "Exit",
+                    },
+                    new BasicButton() {
+                        Height = 100,
+                        Width = 100,
+                        Text = "Resume",
+                    },
+                    new BasicButton() {
+                        Height = 100,
+                        Width = 100,
+                        Text = "Retry",
+                    }
+                ]
+            }
+        ];
+    }
+
+    protected override void PopIn() => this.FadeIn(TRANSITION_DURATION, Easing.In);
+    protected override void PopOut() => this.FadeOut(TRANSITION_DURATION, Easing.In);
+
+    // Don't let touch down events through the overlay or people can touch particle while paused.
+    protected override Boolean OnTouchDown(TouchDownEvent e) => true;
+
+}
