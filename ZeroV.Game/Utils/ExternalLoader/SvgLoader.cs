4a21bb41ea52fcba89b4afc45b84c4f98f1581ed
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2025-01-21 22:49:43 +0800

    Add `SvgLoader`

---
+++ ZeroV.Game/Utils/ExternalLoader/SvgLoader.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Textures;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+using SkiaSharp;
+
+using Svg.Skia;
+
+namespace ZeroV.Game.Utils.ExternalLoader;
+internal class SvgLoader : IDisposable {
+    private Boolean disposedValue = false;
+    private Image<Rgba32> image;
+    private TextureUpload upload;
+
+    public Texture? Texture { get; init; }
+
+    public SvgLoader(FileInfo file, IRenderer renderer) {
+        using SKSvg svg = new();
+        svg.Load(file.FullName);
+        SKRect bounds = svg.Picture.CullRect;
+        using SKBitmap bitmap = new((Int32)bounds.Width, (Int32)bounds.Height, SKColorType.Rgba8888, SKAlphaType.Premul);
+        using SKCanvas canvas = new(bitmap);
+        canvas.Clear(SKColors.Transparent);
+        // canvas.Scale(1,1);
+        // CullRect may not be at (0, 0)
+        canvas.Translate(-bounds.Left, -bounds.Top);
+        canvas.DrawPicture(svg.Picture);
+        canvas.Flush();
+
+        this.image = Image.LoadPixelData<Rgba32>(bitmap.Bytes, bitmap.Width, bitmap.Height);
+        this.upload = new(this.image);
+        this.Texture = renderer.CreateTexture(this.image.Width, this.image.Height);
+        this.Texture.SetData(this.upload);
+    }
+
+    public void Dispose() {
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(Boolean disposing) {
+        if (!this.disposedValue) {
+            if (disposing) {
+                this.Texture?.Dispose();
+                this.image.Dispose();
+            }
+            this.disposedValue = true;
+        }
+    }
+}
