2992e0cdd462836c60569e416138a968e7d4f23a
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2025-03-10 18:21:19 +0800

    Add `BasePreferenceListItem` and `DropdownListItem`

---
+++ ZeroV.Game/Screens/Preference/ListItems/BasePreferenceListItem.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+
+using ZeroV.Game.Configs;
+
+namespace ZeroV.Game.Screens.Preference.ListItems;
+
+public abstract partial class BasePreferenceListItem<T> : CompositeDrawable {
+    public required ZeroVSetting Setting { get; init; }
+
+    public required String LabelText { get; init; }
+
+    public abstract Bindable<T> Current { get; }
+
+    [BackgroundDependencyLoader]
+    private void load(ZeroVConfigManager configManager) {
+        this.RelativeSizeAxes = Axes.X;
+        this.AutoSizeAxes = Axes.Y;
+        this.InternalChild = new Container {
+            RelativeSizeAxes = Axes.X,
+            AutoSizeAxes = Axes.Y,
+            Children = [
+                new Box {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Colour4.DarkGray,
+                },
+                new ZeroVSpriteText {
+                    Padding = new(24),
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    Text = this.LabelText,
+                    FontSize = 30,
+                },
+                new Container {
+                    Anchor = Anchor.CentreRight,
+                    Origin = Anchor.CentreRight,
+                    Padding = new(24),
+                    Children = this.LoadInputController(),
+                },
+            ],
+        };
+        configManager.BindWith(this.Setting, this.Current);
+    }
+
+    protected abstract IReadOnlyList<Drawable> LoadInputController();
+}
---
+++ ZeroV.Game/Screens/Preference/ListItems/DropdownListItem.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.UserInterface;
+
+namespace ZeroV.Game.Screens.Preference.ListItems;
+
+public partial class DropdownListItem<T> : BasePreferenceListItem<T> {
+    public override Bindable<T> Current => this.dropdown.Current;
+
+    private BasicDropdown<T> dropdown = null!;
+
+    protected override IReadOnlyList<Drawable> LoadInputController() {
+        this.dropdown = new BasicDropdown<T> {
+            Anchor = Anchor.CentreRight,
+            Origin = Anchor.CentreRight,
+        };
+        return [this.dropdown];
+    }
+}
