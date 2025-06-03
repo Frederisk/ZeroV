ede3bd2c59c4ec0f60fc22748994c1fb0161ae6c
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2025-06-03 10:34:16 +0800

    Update screen orientation and override method
    Changed the screen orientation to SensorLandscape for better flexibility. Added an override for `SetOrientationBis` to avoid unexpected `ScreenOrientation` changes. See [this issue](https://github.com/ppy/osu-framework/issues/6576).

--- ZeroV.Android/ZeroVActivity.cs
+++ ZeroV.Android/ZeroVActivity.cs
@@ -7,7 +7,13 @@ using ZeroV.Game;
 
 namespace ZeroV.Android;
 
-[Activity(ConfigurationChanges = DEFAULT_CONFIG_CHANGES, Exported = true, LaunchMode = DEFAULT_LAUNCH_MODE, MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape)]
+[Activity(ConfigurationChanges = DEFAULT_CONFIG_CHANGES, Exported = true, LaunchMode = DEFAULT_LAUNCH_MODE, MainLauncher = true, ScreenOrientation = ScreenOrientation.SensorLandscape)]
 public class ZeroVActivity : AndroidGameActivity {
+
     protected override osu.Framework.Game CreateGame() => new ZeroVGame();
+
+    public override void SetOrientationBis(System.Int32 p0, System.Int32 p1, System.Boolean p2, System.String? p3) {
+        // Do nothing here to avoid setting orientation by SDL.
+        // base.SetOrientationBis(p0, p1, p2, p3);
+    }
 }
